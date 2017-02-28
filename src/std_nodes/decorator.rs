f9aeb019b815b0e02850067c7503f90c1957e298
Nathan Kent <nate@nkent.net>
2017-02-28 01:53:24 -0500

    All standard decorator nodes are done

--- src/std_nodes/decorator.rs
+++ src/std_nodes/decorator.rs
@@ -54,3 +54,165 @@ impl<T: Sync> Node<T> for Decorator<T>
 		self.status
 	}
 }
+
+/// Implements a node that will reset its child after the child succeeds or fails
+pub struct Reset<T: Sync>
+{
+	/// Child node
+	child: Box<Node<T>>,
+
+	/// Optional number of times to do the reset
+	attempt_limit: Option<u32>,
+
+	/// Number of times the child has been reset
+	attempts: u32,
+
+	/// Current status of this node
+	status: Status, // Is this needed? Why not just use child status?
+}
+impl<T: Sync> Reset<T>
+{
+	/// Creates a new Reset node that will reset the child indefinitely
+	pub fn new(child: Box<Node<T>>) -> Reset<T>
+	{
+		Reset {
+			child: child,
+			attempt_limit: None,
+			attempts: 0,
+			status: Status::Running,
+		}
+	}
+
+	/// Creates a new Reset node that will reset the child a limited number of times
+	pub fn with_limit(child: Box<Node<T>>, limit: u32) -> Reset<T>
+	{
+		Reset {
+			child: child,
+			attempt_limit: Some(limit),
+			attempts: 0,
+			status: Status::Running,
+		}
+	}
+}
+impl<T: Sync> Node<T> for Reset<T>
+{
+	fn tick(&mut self, world: &mut T) -> Status
+	{
+		// First, get the last status of the child
+		let child_last_status = (*self.child).status();
+
+		// If the child wasn't Running, we need to reset it... if the count allows
+		let reset = child_last_status != Status::Running
+			&& (self.attempt_limit == None
+					|| self.attempt_limit.unwrap() > self.attempts);
+		if reset {
+			// Theoretically, this could overflow if there is no attempt limit. But, if
+			// does, then the user really didn't plan - the node should never tick that
+			// many times in any situation.
+			(*self.child).reset();
+			self.attempts += 1;
+		}
+
+		// Now set our status to the child's
+		self.status = (*self.child).tick(world);
+
+		// We're done
+		self.status
+	}
+
+	fn reset(&mut self)
+	{
+		// Reset our own status
+		self.status = Status::Running;
+		self.attempts = 0;
+
+		// Reset the child
+		(*self.child).reset();
+	}
+
+	fn status(&self) -> Status
+	{
+		self.status
+	}
+}
+
+/// Implements a node that will reset its child after the child fails
+pub struct Retry<T: Sync>
+{
+	/// Child node
+	child: Box<Node<T>>,
+
+	/// Optional number of times to do the reset
+	attempt_limit: Option<u32>,
+
+	/// Number of times the child has been reset
+	attempts: u32,
+
+	/// Current status of this node
+	status: Status, // Is this needed? Why not just use child node?
+}
+impl<T: Sync> Retry<T>
+{
+	/// Creates a new Retry node that will retry the child indefinitely
+	pub fn new(child: Box<Node<T>>) -> Retry<T>
+	{
+		Retry {
+			child: child,
+			attempt_limit: None,
+			attempts: 0,
+			status: Status::Running,
+		}
+	}
+
+	/// Creates a new Retry node that will retry the child a limited number of times
+	pub fn with_limit(child: Box<Node<T>>, limit: u32) -> Retry<T>
+	{
+		Retry {
+			child: child,
+			attempt_limit: Some(limit),
+			attempts: 0,
+			status: Status::Running,
+		}
+	}
+}
+impl<T: Sync> Node<T> for Retry<T>
+{
+	fn tick(&mut self, world: &mut T) -> Status
+	{
+		// First, get the last status of the child
+		let child_last_status = (*self.child).status();
+
+		// If the child failed, we need to retry it... if the count allows
+		let reset = child_last_status == Status::Failed
+			&& (self.attempt_limit == None
+					|| self.attempt_limit.unwrap() > self.attempts);
+		if reset {
+			// Theoretically, this could overflow if there is no attempt limit. But, if
+			// does, then the user really didn't plan - the node should never tick that
+			// many times in any situation.
+			(*self.child).reset();
+			self.attempts += 1;
+		}
+
+		// Now set our status to the child's
+		self.status = (*self.child).tick(world);
+
+		// We're done
+		self.status
+	}
+
+	fn reset(&mut self)
+	{
+		// Reset our own status
+		self.status = Status::Running;
+		self.attempts = 0;
+
+		// Reset the child
+		(*self.child).reset();
+	}
+
+	fn status(&self) -> Status
+	{
+		self.status
+	}
+}
--- src/std_nodes/mod.rs
+++ src/std_nodes/mod.rs
@@ -7,9 +7,13 @@ pub use self::decorator::Reset;
 pub use self::decorator::Retry;
 pub use self::action::Action;
 pub use self::condition::Condition;
+pub use self::constants::AlwaysFail;
+pub use self::constants::AlwaysSucceed;
+pub use self::constants::AlwaysRunning;
 
 mod sequence;
 mod selector;
 mod decorator;
 mod action;
 mod condition;
+mod constants;

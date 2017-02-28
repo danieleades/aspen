1f532a7d93861a7dae10e59d66a3299eb85fc81b
Nathan Kent <nate@nkent.net>
2017-02-28 02:05:57 -0500

    Added constant node types

---
+++ src/std_nodes/constants.rs
@@ -0,0 +1,100 @@
+//! Nodes that have a constant, well-defined behavior
+use std::marker::PhantomData;
+use node::Node;
+use status::Status;
+
+/// Implements a node that always returns that it has failed
+#[derive(PartialEq, Eq, Debug, Copy, Clone, Default)]
+pub struct AlwaysFail<T: Sync>
+{
+	pd: PhantomData<T>,
+}
+impl<T: Sync> AlwaysFail<T>
+{
+	/// Construct a new AlwaysFail node
+	pub fn new() -> AlwaysFail<T>
+	{
+		AlwaysFail { pd: PhantomData }
+	}
+}
+impl<T: Sync> Node<T> for AlwaysFail<T>
+{
+	fn tick(&mut self, _: &mut T) -> Status
+	{
+		Status::Failed
+	}
+
+	fn reset(&mut self)
+	{
+		// No-op
+	}
+
+	fn status(&self) -> Status
+	{
+		Status::Failed
+	}
+}
+
+/// Implements a node that always returns that it has succeeded
+#[derive(PartialEq, Eq, Debug, Copy, Clone, Default)]
+pub struct AlwaysSucceed<T: Sync>
+{
+	pd: PhantomData<T>,
+}
+impl<T: Sync> AlwaysSucceed<T>
+{
+	/// Construct a new AlwaysSucceed node
+	pub fn new() -> AlwaysSucceed<T>
+	{
+		AlwaysSucceed { pd: PhantomData }
+	}
+}
+impl<T: Sync> Node<T> for AlwaysSucceed<T>
+{
+	fn tick(&mut self, _: &mut T) -> Status
+	{
+		Status::Succeeded
+	}
+
+	fn reset(&mut self)
+	{
+		// No-op
+	}
+
+	fn status(&self) -> Status
+	{
+		Status::Succeeded
+	}
+}
+
+/// Implements a node that always returns that it is currently running
+#[derive(PartialEq, Eq, Debug, Copy, Clone, Default)]
+pub struct AlwaysRunning<T: Sync>
+{
+	pd: PhantomData<T>,
+}
+impl<T: Sync> AlwaysRunning<T>
+{
+	/// Construct a new AlwaysRunning node
+	pub fn new() -> AlwaysRunning<T>
+	{
+		AlwaysRunning { pd: PhantomData }
+	}
+}
+impl<T: Sync> Node<T> for AlwaysRunning<T>
+{
+	fn tick(&mut self, _: &mut T) -> Status
+	{
+		Status::Running
+	}
+
+	fn reset(&mut self)
+	{
+		// No-op
+	}
+
+	fn status(&self) -> Status
+	{
+		Status::Running
+	}
+}

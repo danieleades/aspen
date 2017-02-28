71bd99cc86af01eae8ca23bb121b5ce5d35cb8c8
Nathan Kent <nate@nkent.net>
2017-02-28 01:10:15 -0500

    Picked out standard node types
    This seems like a good set of nodes to consider to be standard. Some may
    be added when features are considered.

--- src/std_nodes/mod.rs
+++ src/std_nodes/mod.rs
@@ -2,6 +2,14 @@
 
 pub use self::sequence::Sequence;
 pub use self::selector::Selector;
+pub use self::decorator::Decorator;
+pub use self::decorator::Reset;
+pub use self::decorator::Retry;
+pub use self::action::Action;
+pub use self::condition::Condition;
 
 mod sequence;
 mod selector;
+mod decorator;
+mod action;
+mod condition;

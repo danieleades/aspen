b25815ea76dd4a1d9db025fa177a1633f1a20368
Nathan Kent <nkent2@cs.rochester.edu>
2017-03-01 17:39:07 -0500

    Began working on the Action node

--- src/std_nodes/action.rs
+++ src/std_nodes/action.rs
@@ -1,4 +1,78 @@
 //! Nodes that run their a function in another thread
+use std::thread;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+use node::Node;
+use status::Status;
 
-// TODO
-pub struct Action;
+/// Implements a standard action node.
+pub struct Action<T: Send + Sync + 'static>
+{
+	/// Function that will be run to determine the status
+	func: Arc<Fn(Arc<T>) -> bool + Send + Sync>,
+
+	/// Handle to the thread running the function
+	thread_handle: Option<thread::JoinHandle<Status>>,
+
+	/// Flag that notifies this object that the work thread has completed
+	flag: Arc<AtomicBool>,
+
+	/// Status returned by the last tick
+	status: Status,
+}
+impl<T: Send + Sync + 'static> Action<T>
+{
+	/// Launches a new worker thread to run the function
+	fn start_thread(&mut self, world: &Arc<T>)
+	{
+		// Make sure our flag is set to false
+		self.flag.store(false, Ordering::SeqCst);
+
+		// Then boot up the thread
+		let flag_clone = self.flag.clone();
+		let func_clone = self.func.clone();
+		let world_clone = world.clone();
+		let thread_handle = thread::spawn(move || {
+			// Run the function
+			let res = (func_clone)(world_clone);
+
+			// Set the flag so the main thread knows we're done
+			flag_clone.store(true, Ordering::SeqCst);
+
+			// Return the status
+			if res { Status::Succeeded } else { Status::Failed }
+		});
+
+		// Store the handle for later
+		self.thread_handle = Some(thread_handle);
+	}
+}
+impl<T: Send + Sync + 'static> Node<T> for Action<T>
+{
+	fn tick(&mut self, world: &Arc<T>) -> Status
+	{
+		// If we don't have a thread running, we need to start one
+		if self.thread_handle.is_none() {
+			self.start_thread(world);
+		}
+
+		// There is a thread running - get its status
+		self.status = if !self.flag.load(Ordering::SeqCst) {
+			Status::Running
+		} else {
+			// TODO
+			Status::Running
+		};
+		Status::Running
+	}
+
+	fn reset(&mut self)
+	{
+		// TODO
+	}
+
+	fn status(&self) -> Status
+	{
+		self.status
+	}
+}

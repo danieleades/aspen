59206e4fba41475fe1dd46a2d555ae12059ef162
Nathan Kent <nate@nkent.net>
2017-03-02 13:42:34 -0500

    Added message definition and fixed build script

--- build.rs
+++ build.rs
@@ -1,11 +1,13 @@
-use std::env;
-use std::path::PathBuf;
+#[cfg(feature = "messages")]
+extern crate lcm_gen;
 
-fn main()
+#[cfg(feature = "messages")]
+fn lcm_gen()
 {
+	use std::path::PathBuf;
+
 	// All of our messages will be in the "msg" directory
 	let mut lcm_source_dir : PathBuf = env!("CARGO_MANIFEST_DIR").into();
-	lcm_source_dir.pop();
 	lcm_source_dir.push("msg");
 
 	// Specify when this script needs to be rerun
@@ -15,4 +17,15 @@ fn main()
 	lcm_gen::LcmGen::new()
 	                .add_directory(lcm_source_dir)
 	                .run();
+}
+
+#[cfg(not(feature = "messages"))]
+fn lcm_gen()
+{
+	// No-op
+}
+
+fn main()
+{
+	lcm_gen();
 }
